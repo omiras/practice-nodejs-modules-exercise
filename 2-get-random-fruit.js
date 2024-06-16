5fdd2abb3915fd3d6df6811a293861a872a76c7f
VanyaMark <markova.vanya@gmail.com>
2024-06-16 15:59:55 +0200

    exercise 2 done

--- 2-get-random-fruit.js
+++ 2-get-random-fruit.js
@@ -2,3 +2,9 @@ const frutas = ["Manzana", "Banana", "Pera", "Naranja", "Uva", "Melón", "Sandí
     "Cereza", "Ciruela", "Mango", "Limón", "Mandarina", "Granada", "Higo", "Dátil", "Albaricoque", "Melocotón"];
 
 // importa el módulo "utils.js" y usa el método getRandomElementFromArray adecuadamente
+
+const {getRandomElementFromArray} = require('./mis-utilidades/utils');
+
+console.log(getRandomElementFromArray(frutas))
+console.log(getRandomElementFromArray(frutas))
+console.log(getRandomElementFromArray(frutas))

f2ba8bafb34ce170aa8366510105c12002f87a91
VanyaMark <markova.vanya@gmail.com>
2024-06-16 15:54:02 +0200

    first exercise done

--- 1-os-module.js
+++ 1-os-module.js
@@ -1,2 +1,17 @@
 // Leer README
 
+const os = require('os');
+
+const hostname = os.hostname()
+
+console.log(`Nombre del equipo: ${hostname}`);
+
+const osVersion = os.version();
+
+console.log(`Edición/Versión del sistema operativo: ${osVersion}`);
+
+// Obtener la memoria RAM total en GB
+
+const memoryGb = Math.ceil(os.totalmem()/ (1024 ** 3));
+
+console.log(`Memoria RAM total: ${memoryGb}`);
